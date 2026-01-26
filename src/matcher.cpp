b7706ce9ef425aa49e4097d0c027767eabe61e80
HungHong <176441814+HungHHong@users.noreply.github.com>
2026-01-26 15:39:13 -0500

    refactor: update file structure for task A and B

---
+++ src/matcher.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello matcher" << endl;
+    return 0;
+}
---
+++ src/verifier.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "hello verifier" << endl;
+    return 0;
+}
